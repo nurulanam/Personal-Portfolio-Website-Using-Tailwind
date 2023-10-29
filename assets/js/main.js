c2528b6d09e61431d2794bf10694b98f57f57125
Nurul Anam <nurulanam.bd@gmail.com>
2023-10-30 00:34:32 +0600

    Feat: My style section added. Fix: case study bg

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,20 +166,7 @@ gsap.to('.services-desctiption span', {
         scrub: 2,
     }
 })
-gsap.to('.case-study-section', {
-    background: '#F44336',
-    scrollTrigger: {
-      trigger: '.case-study-section',
-      scroller: 'main',
-      start: 'top 75%',
-      end: '+=10%',
-      scrub: 2,
-    }
-  });
-gsap.from('.case-study-section', {
-    marginLeft: '30%',
-    marginRight: '30%',
-  })
+
 gsap.to('.case-study-section', {
     marginLeft: 0,
     marginRight: 0,
@@ -187,9 +174,77 @@ gsap.to('.case-study-section', {
     scrollTrigger: {
       trigger: '.case-study-section',
       scroller: 'main',
-      start: 'top 60%',
+      start: 'top 50%',
       end: '+=10%',
       scrub: 2,
     }
   });
+
+    function initMarquee() {
   
+      // Kill other animations
+      animations.forEach(animation => animation.progress(0).kill())
+    
+      // Marquee speed (pixels per second)
+      let velocity = 150;
+      
+      let offset = 0
+      let itemWidth = 0
+      let rowWidth = 0
+      
+      let marqueeItems = gsap.utils.toArray('.my-style-item')
+    
+      // Calculate row width
+      marqueeItems.forEach(e => {
+        rowWidth += e.getBoundingClientRect().width
+      })
+      
+      // Animation Loop
+      marqueeItems.forEach((e, i) => {
+    
+        // Reset item positions
+        gsap.set(e, {x: 0})
+        
+        itemWidth = e.getBoundingClientRect().width
+        
+        let tl = new gsap.timeline({ repeat: -1 });
+        
+        // Animate item to end of row
+        tl.to(e, {
+          ease: "none",
+          duration: ((rowWidth - offset - itemWidth) / velocity),
+          x: (rowWidth - offset - itemWidth),
+        });
+            
+        // Send item to beginning
+        tl.to(e, {
+          ease: "none",
+          duration: 0,
+          x: ((offset + itemWidth) * -1)
+        })
+        
+        // Animate to original position
+        tl.to(e, {
+          ease: "none",
+          duration: ((offset + itemWidth) / velocity),
+          x: 0
+        })
+        
+        // Increment offset
+        offset += itemWidth
+        
+        animations.push(tl)
+      })
+    }
+    
+    let animations = []
+    
+    initMarquee()
+     
+    var timer
+    function handleResize() {
+      clearTimeout(timer)
+      timer = setTimeout(initMarquee, 500)
+    }
+    
+    window.addEventListener('resize', handleResize)
